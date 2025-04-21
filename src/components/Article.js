863a490619c11f4c8855134d4d5cbf0076631052
Silva-NK <kiagonsuzanne@gmail.com>
2025-04-21 18:08:41 +0300

    Minor Fixes.

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,15 +2,19 @@ import React from "react";
 
 const getReadEmojis = (minutes) => {
     return minutes < 30
-    ? '☕️'.repeat(Math.ceil(minutes /5))+ ` ${minutes} min read.`
-    : '🍱'.repeat(Math.ceil(minutes /10))+ ` ${minutes} min read.`;
+    ? '☕️'.repeat(Math.ceil(minutes /5))+ `${minutes} min read.`
+    : '🍱'.repeat(Math.ceil(minutes /10))+ `${minutes} min read.`;
 };
 
 function Article({title, date = "January 1, 1970", preview, minutes}) {
+    const readingTime = typeof minutes === "number"
+    ? ` • ${getReadEmojis(minutes)}`
+    : "";
+    
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}  • {getReadEmojis(minutes)}</small>
+            <small>{date}{readingTime}</small>
             <p>{preview}</p>
         </article>
     );
